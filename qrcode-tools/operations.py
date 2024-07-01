d0bd2bf174d01387479ecb0b617b1047338d4656
spathan <spathan@fortinet.com>
2024-07-01 17:39:44 +0530

    Added support to specify name or path of the file in /tmp directory to read the QR(Bar) code

--- qrcode-tools/operations.py
+++ qrcode-tools/operations.py
@@ -5,7 +5,6 @@ Copyright (c) 2024 Fortinet Inc
 Copyright end
 """
 
-
 import logging
 import zxingcpp
 import cv2
@@ -14,7 +13,6 @@ from connectors.core.connector import get_logger, ConnectorError
 from integrations.crudhub import make_request
 from connectors.cyops_utilities.builtins import download_file_from_cyops
 
-
 logger = get_logger('qrcode-tools')
 logger.setLevel(logging.DEBUG)
 
@@ -22,8 +20,10 @@ logger.setLevel(logging.DEBUG)
 Utilities: support functions
 """
 
+
 def _print_json(json_object):
     return json.dumps(json_object, indent=4)
+
 
 def _get_file_path(file_id):
     iri_type = 'attachment'
@@ -47,26 +47,32 @@ def _get_file_path(file_id):
     file_path = "{0}/{1}".format('/tmp', res['cyops_file_path'])
     return file_path
 
+
 """
 Operations: connector's actions implementation
 """
 
+
 def read_qr_code(config, params):
     """Reads QRcode from an image file"""
     codes = []
-    img = cv2.imread(_get_file_path(params.get("file_iri")))
+    if params.get('type') == 'File Path':
+        file_id = str(params.get("file_iri"))
+        file_path = file_id if file_id.startswith('/tmp') else '/tmp/{0}'.format(file_id)
+    else:
+        file_path = _get_file_path(params.get("file_iri"))
+    img = cv2.imread(file_path)
     results = zxingcpp.read_barcodes(img)
     for result in results:
         codes.append({
             "text": f'{result.text}',
             "format": f'{result.format}',
             "content": f'{result.content_type}',
-            "position": f'{result.position}'.replace('\u0000','')
+            "position": f'{result.position}'.replace('\u0000', '')
         })
     logger.debug(_print_json(codes))
     if len(codes) == 0:
         logger.warning("No QR Code found")
-        return {'status':'success', 'message':'No QR Code found'}
+        return {'status': 'success', 'message': 'No QR Code found'}
     else:
         return codes
-
---
+++ qrcode-tools/release_notes.md
@@ -0,0 +1,2 @@
+#### What's Improved
+ - Added support to specify name or path of the file in /tmp directory to read the QR(Bar) code.
